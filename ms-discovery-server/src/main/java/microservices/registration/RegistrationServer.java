8f8159447263ceab76879d23724944d242886115
Christan Vrey <christan.vrey@vcontractor.co.za>
2017-10-17 22:32:47 +0200

    Lab 8 Spring Cert

--- ms-discovery-server/src/main/java/microservices/registration/RegistrationServer.java
+++ ms-discovery-server/src/main/java/microservices/registration/RegistrationServer.java
@@ -3,7 +3,6 @@ package microservices.registration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -11,8 +10,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * <p>
  * Start this process FIRST.
  */
-// TODO-01: Add annotations to make this a Eureka server using Spring Boot
-//
+@SpringBootApplication
+@EnableEurekaServer
 // Older versions of STS/Eclipse may generate an httpMapperProperties deprecated
 // warning, which can safely be ignored.
 public class RegistrationServer {
@@ -24,17 +23,12 @@ public class RegistrationServer {
 	 *            Program arguments - ignored.
 	 */
 	public static void main(String[] args) {
-		// TODO-02: Configure this to run using SpringApplication
+		SpringApplication.run(RegistrationServer.class, args);
+	}
 
-		// TODO-03: Run this as a Spring Boot application by doing
-		// right-click -> Run As -> Spring Boot App
-		// Then open the server's Dashboard at http://localhost:1111/ in
-		// a browser
-		//
-		// TODO-04: Why is this server's port 1111? Where was this configured?
-		//
-		// TODO-05: Now go to the ms-account-server project - follow its
-		// TO DO instructions.
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RegistrationServer.class);
 	}
 
 }
